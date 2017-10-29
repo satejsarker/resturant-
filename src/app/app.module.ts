1c1036d8622b68384221399aa0c3add41bf7290e
keysarker@outlook.com <keysarker@outlook.com>
2017-10-29 18:17:49 +0600

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-
+import { HttpModule } from '@angular/http';
+import { baseURL } from '../shared/baseurl';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { DishProvider } from '../providers/dish/dish';
+import { LeaderProvider } from '../providers/leader/leader';
+import { PromotionProvider } from '../providers/promotion/promotion';
+import { ProcessHttpmsgProvider } from '../providers/process-httpmsg/process-httpmsg';
 
 @NgModule({
   declarations: [
@@ -18,6 +23,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
+    HttpModule,
+
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -28,7 +35,13 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    DishProvider,
+    DishProvider,
+    LeaderProvider,
+    PromotionProvider,
+    ProcessHttpmsgProvider,
+    { provide: 'BaseURL', useValue: baseURL }
   ]
 })
 export class AppModule {}
---
+++ src/providers/promotion/promotion.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { Promotion } from '../../shared/promotion';
+import { Observable } from 'rxjs/Observable';
+import { Http, Response } from '@angular/http';
+import { baseURL } from '../../shared/baseurl';
+import { ProcessHttpmsgProvider } from '../process-httpmsg/process-httpmsg';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/delay';
+import 'rxjs/add/operator/catch';
+@Injectable()
+export class PromotionProvider {
+
+  constructor(public http: Http,
+              private processHTTPMsgService: ProcessHttpmsgProvider) { }
+
+  getPromotions(): Observable<Promotion[]> {
+    return this.http.get(baseURL + 'promotions')
+                    .map(res => { return this.processHTTPMsgService.extractData(res); })
+                    .catch(error => { return this.processHTTPMsgService.handleError(error); });
+  }
+
+  getPromotion(id: number): Observable<Promotion> {
+    return  this.http.get(baseURL + 'promotions/'+ id)
+                    .map(res => { return this.processHTTPMsgService.extractData(res); })
+                    .catch(error => { return this.processHTTPMsgService.handleError(error); });
+  }
+
+  getFeaturedPromotion(): Observable<Promotion> {
+    return this.http.get(baseURL + 'promotions?featured=true')
+                    .map(res => { return this.processHTTPMsgService.extractData(res)[0]; })
+                    .catch(error => { return this.processHTTPMsgService.handleError(error); });
+  }
+
+}
