58f73ba86644245916c898428fd473823d3ac2a0
keysarker@outlook.com <keysarker@outlook.com>
2017-11-17 18:56:18 +0600

    added formcontrol and reservation work

--- src/pages/reservation/reservation.ts
+++ src/pages/reservation/reservation.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams,ViewController } from 'ionic-angular';
+import{Validators,FormBuilder,FormGroup} from '@angular/forms';
 
 /**
  * Generated class for the ReservationPage page.
@@ -14,14 +15,26 @@ import { IonicPage, NavController, NavParams,ViewController } from 'ionic-angula
   templateUrl: 'reservation.html',
 })
 export class ReservationPage {
+  reservation:FormGroup
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public viewController:ViewController) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public viewController:ViewController,
+  private formBuilder: FormBuilder) {
+    this.reservation=this.formBuilder.group({
+      guests : 3,
+      smoking:false,
+      dateTime:['',Validators.required]
+
+    })
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ReservationPage');
   }
   dismiss(){
+    this.viewController.dismiss();
+  }
+  onSubmit(){
+    console.log(this.reservation.value);
     this.viewController.dismiss();
   }
 
